82af5e6aeccaf96c50ac8cc87647e5b29af6bee1
AlexCsanyi <alex.csanyi@gmail.com>
2019-10-10 07:17:28 +0100

    started to write requests to db

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const PORT = 4000;
 const mongoose = require("mongoose");
+const todoRoutes = express.Router();
+
+let Todo = require("./todo.model");
 
 const app = express();
 app.use(cors());
@@ -17,6 +20,18 @@ const connection = mongoose.connection;
 connection.once("open", function() {
   console.log("mongoDB connection is established successfully");
 });
+
+todoRoutes.route("/").get(function(req, res) {
+  Todo.find(function(err, todos) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
+
+app.use("/todos", todoRoutes);
 
 app.listen(PORT, function() {
   console.log("Server is running on Port: " + PORT);
