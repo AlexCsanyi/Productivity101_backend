1d48e2bf228319e8a27408ec2900706e025fef76
AlexCsanyi <alex.csanyi@gmail.com>
2019-10-15 06:58:14 +0100

    added get by id, and add new item end points

--- server.js
+++ server.js
@@ -31,6 +31,24 @@ todoRoutes.route("/").get(function(req, res) {
   });
 });
 
+todoRoutes.route("/:id").get(function(req, res) {
+  let id = req.params.id;
+  Todo.findById(id, function(err, todo) {
+    res.json(todo);
+  });
+});
+
+todoRoutes.route("/add").post(function(req, res) {
+  let todo = new Todo(req.body);
+  todo.save
+    .then(todo => {
+      res.status(200).json({ todo: "todo added successfully" });
+    })
+    .catch(err => {
+      res.status(400).send("adding new todo failed.");
+    });
+});
+
 app.use("/todos", todoRoutes);
 
 app.listen(PORT, function() {
